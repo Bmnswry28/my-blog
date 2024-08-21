be54ba17fcbb7c25ad76f5a9f2352461547e7b84
abbas <bmnswry27@gmail.com>
2024-08-21 16:14:21 +0330

    add class Profile

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    bio=models.TextField(max_length=500,null=True,blank=True)
+    profile_pic=models.ImageField('profile_images/',null=True,blank=True)
+    def __str__(self):
+        return self.user.username
 class posts(models.Model):
     title=models.CharField(max_length=255)
     content=models.TextField()
