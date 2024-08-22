65d68500a98082965b45213c4bf8c729c6e9e572
abbas <bmnswry27@gmail.com>
2024-08-22 19:08:50 +0330

    add static

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
+def index(request):
+    Posts = Post.objects.all().order_by('publishedDate')
+    context ={
+        'post':Posts
+    }
+    return render(request, 'index.html', context)
 # Create your views here.
