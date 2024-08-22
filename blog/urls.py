a00b47fceef02c20f825462ebc57bbb989bdc4cb
abbas <bmnswry27@gmail.com>
2024-08-23 00:06:43 +0330

    url

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-        path('search/', search_results, name='search_results'),
+    path('search', search_results, name='search_results'),
+    path('archive', archive_view, name='archive_list'),
+    path('about', AboutView, name='about'),
+    path('titles', post_titles_view, name='post_titles'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-from .models import *
+from .models import Post
+from django.db.models import Q
 
 def index(request):
-    Posts = Post.objects.all().order_by('publishedDate')
-    context = {
-        'post':Posts
-    }
-    return render(request, 'index.html', context)
-# Create your views here.
+    posts = Post.objects.all()  
+    return render(request, 'index.html', {'posts': posts})
+
 def search_results(request):
-    query = request.GET.get('q', '')
-    results = Post.objects.filter(title__icontains=query)
-    return render(request, 'search_results.html', {'posts': results, 'query': query})
+    query = request.GET.get('q')
+    if query:
+        results = Post.objects.filter(
+            Q(title__icontains=query) | Q(content__icontains=query)
+        )
+    else:
+        results = Post.objects.none()
+    return render(request, 'search_results.html', {'results': results, 'query': query})
+def archive_view(request):
+    posts = Post.objects.all().order_by('-publishedDate')  # استفاده از فیلد صحیح
+    return render(request, 'archive.html', {'posts': posts})
+
+def detail_view(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'detail.html', {'post': post})
+def AboutView(request):
+    return render(request, 'about.html')
+
+def post_titles_view(request):
+    titles = Post.objects.all().order_by('-publishedDate')  
+    return render(request, 'title.html', {'titles': titles})
