6553e76db141dae336d00d495f33844ebb04dc3e
abbas <bmnswry27@gmail.com>
2024-08-23 17:08:33 +0330

    ..

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,38 @@
-from django.shortcuts import render
-from .models import Post,Profile
-from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Profile
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.views.generic import ListView, DetailView
 
 def index(request):
     posts = Post.objects.all()  
     return render(request, 'index.html', {'posts': posts})
 
+from django.shortcuts import render
+from .models import Post
+
 def search_results(request):
-    query = request.GET.get('q')
+    query = request.GET.get('q', '')
     if query:
-        results = Post.objects.filter(
-            Q(title__icontains=query) | Q(content__icontains=query)
-        )
+        results = Post.objects.filter(title__icontains=query) | Post.objects.filter(Content__icontains=query)
     else:
         results = Post.objects.none()
+    
     return render(request, 'search_results.html', {'results': results, 'query': query})
+
+
 def archive_view(request):
-    posts = Post.objects.all().order_by('-publishedDate')  # استفاده از فیلد صحیح
+    posts = Post.objects.all().order_by('-publishedDate')
     return render(request, 'archive.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'page.html', {'post': post})
+
 def AboutView(request):
     return render(request, 'about.html')
 
 def post_titles_view(request):
-    titles = Post.objects.all().order_by('-publishedDate')  
+    titles = Post.objects.all().order_by('-publishedDate')
     return render(request, 'title.html', {'titles': titles})
 
 class ProfileListView(ListView):
